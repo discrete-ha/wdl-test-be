5a2f855089b95a8effb6860ad1ac2c827c588785
John <discreteb@gmail.com>
2017-12-18 15:12:05 +0900

    simplified

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,11 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)      
  
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return "hello"
  
 if __name__ == '__main__':
   app.run(debug=True)
-
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
