65e49745919843571be332a16d9a31a90c23c14e
John <discreteb@gmail.com>
2017-12-18 15:09:21 +0900

    add run

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
 import os
- 
+from flask import Flask, render_template
+
 app = Flask(__name__)      
  
 @app.route('/')
@@ -9,3 +9,5 @@ def home():
  
 if __name__ == '__main__':
   app.run(debug=True)
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
